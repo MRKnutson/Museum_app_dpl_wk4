2fff7eea92d71c9caba02a1790b3b50c9912f073
Michael Knutson <mrknutson44@gmail.com>
2021-12-01 16:31:31 -0900

    exhibits rendering

--- client/src/components/Exhibits.js
+++ client/src/components/Exhibits.js
@@ -1,10 +1,39 @@
-import React from "react"
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
 
 const Exhibits = () => {
+  const [exhibits, setExhibits]=useState([]);
+
+  useEffect(()=>{
+    getExhibits();
+  },[]);
+
+  const getExhibits = async () => {
+    try {
+      let res = await axios.get("/api/exhibits");
+      setExhibits(res.data);
+      console.log(res.data);
+    } catch (err) {
+      alert("error getting exhibits: debug");
+    };
+  };
+
+  const renderExhibits = () => {
+    return exhibits.map((exhibit)=>{
+      return(
+        <div key = {exhibit.id}>
+          <h2>Area: {exhibit.area}</h2>
+          <p>Circa: {exhibit.circa}</p>
+        </div>
+      );
+    });
+  };
+
   return(
     <div>
       <h1>Museum of Blah Blah Blah</h1>
-      <p>render exhibits here</p>
+      {renderExhibits()}
     </div>
   )
 };
